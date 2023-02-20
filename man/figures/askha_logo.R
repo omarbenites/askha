2d4e19fe154075713ca6e3f29343ff6baa4c0392
omarbenites <omar_cs_7@hotmail.com>
2023-02-20 16:23:08 -0500

    sticker logo

--- man/figures/askha_logo.R
+++ man/figures/askha_logo.R
@@ -26,3 +26,14 @@ sticker(logo, package="askha",white_around_sticker = TRUE,
         h_fill="#d1f4eeff",h_color = "#000000", p_family = "Righteous",p_color = "black",
         filename = "man/figures/askha_sticker_logo.png"
 )
+
+
+fuzz <- 50
+p <- image_read("man/figures/askha_sticker_logo.png")
+pp <- p %>%
+  image_fill(color = "transparent", refcolor = "white", fuzz = fuzz, point = "+1+1") %>%
+  image_fill(color = "transparent", refcolor = "white", fuzz = fuzz, point = paste0("+", image_info(p)$width-1, "+1")) %>%
+  image_fill(color = "transparent", refcolor = "white", fuzz = fuzz, point = paste0("+1", "+", image_info(p)$height-1)) %>%
+  image_fill(color = "transparent", refcolor = "white", fuzz = fuzz, point = paste0("+", image_info(p)$width-1, "+", image_info(p)$height-1))
+image_write(image = pp, path = "man/figures/askha_sticker_logo.png")
+
